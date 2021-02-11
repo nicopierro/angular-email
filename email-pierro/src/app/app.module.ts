2e4f06a44f29cd0ac796c18eaf1411d5aa1ec1fd
nicopierro <pierro.nicolo@iisgalvanimi.edu.it>
2021-02-11 10:43:19 +0000

    punto 7

--- email-pierro/src/app/app.module.ts
+++ email-pierro/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SentMailComponent } from './sent-mail/sent-mail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SentMailComponent } from './sent-mail/sent-mail.component';
     SentMailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
